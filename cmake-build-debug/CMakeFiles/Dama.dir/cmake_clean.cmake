985ee85387b9a0475706964067269b1b51957b5b
michael <michael@busnet.it>
2022-04-14 00:49:50 +0200

    Added my project

---
+++ cmake-build-debug/CMakeFiles/Dama.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dama.dir/main.cpp.o"
+  "Dama"
+  "Dama.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dama.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
